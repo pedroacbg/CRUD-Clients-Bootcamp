365c0a29451afb2d539502cbcd75f952d8004314
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2021-10-26 21:03:58 -0300

    Update, Delete client

--- src/main/java/com/pedroanjos/crudclients/services/ClientService.java
+++ src/main/java/com/pedroanjos/crudclients/services/ClientService.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pedroanjos.crudclients.dto.ClientDTO;
 import com.pedroanjos.crudclients.entities.Client;
+import com.pedroanjos.crudclients.exceptions.DatabaseException;
 import com.pedroanjos.crudclients.exceptions.ResourceNotFoundException;
 import com.pedroanjos.crudclients.repositories.ClientRepository;
 
@@ -38,6 +43,29 @@ public class ClientService {
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID not found " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
